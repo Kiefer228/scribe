dc0d05d3965057e934f2dc6cb3678dd46a6b1d08
unknown <kiefernobody@gmail.com>
2024-12-25 23:41:58 -0500

    Quill Installed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { EditorStateProvider } from './context/useEditorState'; // Import the provider
 import Editor from './components/Editor';
 import Sidebar from './components/Sidebar';
+import './styles/main.css';
 
 function App() {
     return (
-        <div className="app-container">
-            <Sidebar />
-            <Editor />
-        </div>
+        <EditorStateProvider> {/* Wrap the app in the context provider */}
+            <div className="app-container">
+                {/* Sidebar for metadata and summaries */}
+                <Sidebar />
+
+                {/* Main editor component */}
+                <main className="editor-section">
+                    <Editor />
+                </main>
+            </div>
+        </EditorStateProvider>
     );
 }
 
--- src/Components/Editor.js
+++ src/Components/Editor.js
@@ -1,19 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { useEditorState } from '../context/useEditorState';
 
-function Editor() {
+const Editor = () => {
     const { content, updateContent } = useEditorState();
-    const handleChange = (value) => {
-        updateContent(value);
-    };
 
     return (
         <div className="editor-container">
-            <ReactQuill value={content} onChange={handleChange} />
+            <ReactQuill
+                value={content}
+                onChange={updateContent}
+                theme="snow"
+                placeholder="Start writing your content here..."
+            />
         </div>
     );
-}
+};
 
 export default Editor;
