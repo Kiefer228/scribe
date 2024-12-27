2c971c60b2b2cf2a6dfea67f50415455dfd4ba57
Kiefer228 <38995699+Kiefer228@users.noreply.github.com>
2024-12-27 12:27:10 -0500

    ai analysis cleanup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/App.css';
 import Editor from './Components/Editor';
 import Toolbar from './Components/Toolbar';
@@ -7,6 +7,30 @@ import { EditorStateProvider } from './context/useEditorState';
 import { GoogleDriveProvider } from './context/useGoogleDrive';
 
 function App() {
+  const [moduleState, setModuleState] = useState({
+    width: 600,
+    height: 800,
+    x: 100,
+    y: 100,
+  });
+
+  const handleDragStop = (e, d) => {
+    setModuleState((prevState) => ({
+      ...prevState,
+      x: d.x,
+      y: d.y,
+    }));
+  };
+
+  const handleResizeStop = (e, direction, ref, delta, position) => {
+    setModuleState({
+      width: parseInt(ref.style.width, 10),
+      height: parseInt(ref.style.height, 10),
+      x: position.x,
+      y: position.y,
+    });
+  };
+
   return (
     <GoogleDriveProvider>
       <EditorStateProvider>
@@ -15,12 +39,10 @@ function App() {
           <div className="desktop-layout">
             <Rnd
               className="module"
-              default={{
-                width: 600,
-                height: 800,
-                x: 100,
-                y: 100,
-              }}
+              size={{ width: moduleState.width, height: moduleState.height }}
+              position={{ x: moduleState.x, y: moduleState.y }}
+              onDragStop={handleDragStop}
+              onResizeStop={handleResizeStop}
               bounds="parent"
               enableResizing={{
                 top: true,
