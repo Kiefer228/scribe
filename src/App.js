8ff35ae1090f9d2a71d31807292daa1ea78f1688
Kiefer228 <38995699+Kiefer228@users.noreply.github.com>
2024-12-26 20:53:26 -0500

    Disabled Module resizing

--- src/App.js
+++ src/App.js
@@ -15,11 +15,19 @@ function App() {
           <div className="desktop-layout">
             <Rnd
               className="module editor"
-              default={{ x: 300, y: 100, width: 800, height: 600 }}
+              default={{
+                x: 300,
+                y: 100,
+                width: 800,
+                height: 600,
+              }}
+              disableDragging={false}
+              enableResizing={false} // Disable resizing
+              bounds="parent" // Restrict movement within parent container
             >
               <Editor />
             </Rnd>
-            {/* Additional panels can be added here */}
+            {/* Additional fixed-size panels can be added here */}
           </div>
         </div>
       </EditorStateProvider>
