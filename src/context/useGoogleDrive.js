c78a3485b1d0c4bd797bbbd826aee3daff698114
Kiefer228 <38995699+Kiefer228@users.noreply.github.com>
2024-12-30 13:34:26 -0500

    xx

--- src/context/useGoogleDrive.js
+++ src/context/useGoogleDrive.js
@@ -124,7 +124,7 @@ export const GoogleDriveProvider = ({ children }) => {
                 dispatch({ type: "AUTH_FAIL" });
             }
         }
-
+        
         initializeDrive();
     }, []);
 
