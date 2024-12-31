07345b6fa4949508182cc0e10e56ab23fb52294c
Kiefer228 <38995699+Kiefer228@users.noreply.github.com>
2024-12-31 14:33:57 -0500

    config overrides update

--- config-overrides.js
+++ config-overrides.js
@@ -1,18 +1,27 @@
 const webpack = require("webpack");
 
-module.exports = function override(config) {
-    config.resolve.fallback = {
-        ...config.resolve.fallback,
-        stream: require.resolve("stream-browserify"),
-        buffer: require.resolve("buffer"),
-    };
-
-    config.plugins = (config.plugins || []).concat([
-        new webpack.ProvidePlugin({
-            Buffer: ["buffer", "Buffer"],
-            process: "process/browser",
-        }),
-    ]);
-
-    return config;
+module.exports = {
+  resolve: {
+    fallback: {
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+      buffer: require.resolve("buffer"),
+      process: require.resolve("process/browser"),
+      util: require.resolve("util/"),
+      path: require.resolve("path-browserify"),
+      assert: require.resolve("assert/"),
+      fs: false, // Browser doesn't support fs
+      os: require.resolve("os-browserify/browser"),
+      https: require.resolve("https-browserify"),
+      http: require.resolve("stream-http"),
+      url: require.resolve("url/"),
+      zlib: require.resolve("browserify-zlib"),
+    },
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+      process: "process/browser",
+    }),
+  ],
 };
