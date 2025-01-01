fc40d3324e60cade37d91550e3436bf41f3de3f4
Kiefer228 <38995699+Kiefer228@users.noreply.github.com>
2024-12-31 23:41:11 -0500

    xx

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function AppContent() {
     const [errorMessage, setErrorMessage] = useState(null);
 
     const [moduleConfig, setModuleConfig] = useState({
-        journal: { x: 0, y: 950, isLocked: true },
+        journal: { x: 0, y: 900, isLocked: true },
         editor: { x: 650, y: 75, isLocked: false },
     });
 
